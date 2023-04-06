a0634f8aba642b7e159cd7b4ea32781a8371b043
rksrbbn <rakasantang22@gmail.com>
2023-04-06 15:32:04 +0700

    update

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login (Request $request) {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user)
+        {
+            return response()->json(['message'=>'email/password salah!'], 400);
+        }
+        
+        if (!Hash::check($request->password, $user->password))
+        {
+            return response()->json(['message'=>'email/password salah!'], 400);
+        }
+
+        $token = $user->createToken('access_token');
+        return response()->json([
+            'token' => $token->plainTextToken
+        ]);
+    }
+}
